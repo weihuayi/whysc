b76a36576c52f11fe89e926804db401e6989ab7f
Huayi Wei <weihuayi@xtu.edu.cn>
2021-03-22 11:27:08 +0800

    update

--- include/mesh/color.h
+++ include/mesh/color.h
@@ -1,94 +1,127 @@
 #include<stdio.h>
-#include <iostream>
+#include<iostream>
 #include<list>
 #include<vector>
+#include<set>
 
-using namespace std;
 
 template<typename Mesh>
 void mesh_coloring(Mesh & mesh, int * color)
 {
-    typedef typename Mesh::Toplogy Tp;
+  typedef typename Mesh::Toplogy Toplogy;
 
-    int GD = mesh.geo_dimension();
-    int NN = mesh.number_of_nodes();
-    int NE = mesh.number_of_edges();
+  auto NN = mesh.number_of_nodes();
+  auto NE = mesh.number_of_edges();
 
-    int c = 0;
-    int data[NN];
+  int c = 0;
+  std::vector<int> randVal(NN);
+  std::vector<bool> isMin(NN, true);
 
-    list<int> edge; //没有被删除的边
-    list<int> nColored; //没有被染色的点
-    for(int i=0; i < NE; i++){edge.push_back(i);}
-    for(int i=0; i < NN; i++){nColored.push_back(i);}
+  std::list<int> edges; //没有被删除的边
+  std::list<int> nColored; //没有被染色的点
 
-    //染色
-    while(nColored.size()>0){
-        c++;
-        bool is_min_data[NN];
-        for(int i = 0; i < NN; i++){
-            is_min_data[i] = true; //初始所有未染色点都是最小点
-        }
+  for(int i=0; i < NE; i++)
+  {
+    edges.push_back(i);
+  }
 
-        for(list<int>::iterator i = nColored.begin(); i != nColored.end(); i++){
-            data[*i] = rand();//产生随机数
-        }
+  for(int i=0; i < NN; i++)
+  {
+    nColored.push_back(i);
+  }
 
-        for(list<int>::iterator i = edge.begin(); i != edge.end();){
-            int & end0 = mesh.edge(*i)[0];
-            int & end1 = mesh.edge(*i)[1];
-
-            //判断*i是否为局部最小data值
-            if(color[end0]!=0 || color[end1]!=0){
-                edge.erase(i++);
-            }
-            else{
-                if(data[end0] < data[end1]){
-                    is_min_data[end1] = false;
-                }
-                else{
-                    is_min_data[end0] = false;
-                }
-                i++;
-            }
-        }
-        for(list<int>::iterator i = nColored.begin(); i != nColored.end();){
-            if(is_min_data[*i])
-            {
-                color[*i] = c;
-                nColored.erase(i++);
-            }
-            else{i++;}
-        }
-
+  //染色
+  while(nColored.size()>0)
+  {
+    c++;
+    // 只对没有染色的点进行循环
+    for(const int& index: nColored)
+    {
+      isMin[index] = true; //初始所有未染色点都是最小点
+      randVal[index] = rand();
     }
 
-    //降低颜色数量
-    int cMax = c;
-    Tp node2node;
-    mesh.node_to_node(node2node);
-    auto & nei = node2node.neighbors();
-    auto & loc = node2node.locations();
-
-    while(c>0){
-        for(int i = 0; i < NN; i++){
-            list<int> com;
-            for(int i=0; i < cMax;i++){com.push_back(i+1);}
-            for(int j = loc[i]; j < loc[i+1]; j++){
-                com.remove(color[nei[j]]);
-            }
-            color[i] = *min_element(com.begin(), com.end());
+    for(auto it = edges.begin(); it != edges.end();)
+    {
+      auto e = mesh.edge(*it);
+      //判断*i是否为局部最小data值
+      if(color[e[0]]!=0 || color[e[1]]!=0)
+      {
+        it = edges.erase(it); // 删除当前的边编号, 并移动到下一个边
+      }
+      else
+      {
+        if(randVal[e[0]] < randVal[e[1]])
+        {
+          isMin[e[1]] = false;
         }
-
-        int num = 0;
-        for(int i = 0; i < NN; i++){
-            if(color[i]==cMax){
-                num++;
-            }
+        else
+        {
+          isMin[e[0]] = false;
         }
-
-        if(num==0){cMax -= 1;}
-        c--;
+        it++;
+      }
     }
+
+    for(auto it = nColored.begin(); it != nColored.end();)
+    {
+      if(isMin[*it])
+      {
+        color[*it] = c;
+        it = nColored.erase(it);
+      }
+      else
+      {
+        it++;
+      }
+    }
+
+  }
+
+  //降低颜色数量
+  int cMax = c;
+  Toplogy node2node;
+  mesh.node_to_node(node2node);
+
+  // 尝试用新的 API
+  auto & nei = node2node.neighbors();
+  auto & loc = node2node.locations();
+
+  while(true)
+  {
+    for(int i = 0; i < NN; i++)
+    {
+      std::set<int> com;
+      for(int k=1; k <= cMax; k++)
+      {
+        com.insert(k);
+      }
+
+      for(int j = loc[i]; j < loc[i+1]; j++)
+      {
+          com.erase(color[nei[j]]);
+      }
+      color[i] = *std::min_element(com.begin(), com.end());
+    }
+
+    bool flag = true;
+    for(int i = 0; i < NN; i++)
+    {
+        if( color[i] == cMax )
+        {
+          flag == false;
+          break;
+        }
+    }
+
+    if(flag)
+    {
+      cMax -= 1;
+    }
+    else
+    {
+      break;
+    }
+  }
 };
 
