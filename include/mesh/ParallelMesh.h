f9e53acc8d77a95427120ed0d88ba70a856c09b4
Huayi Wei <weihuayi@xtu.edu.cn>
2021-03-30 17:20:52 +0800

    update

--- include/mesh/ParallelMesh.h
+++ include/mesh/ParallelMesh.h
@@ -28,27 +28,99 @@ public:
   typedef typename Mesh::EdgeIterator EdgeIterator;
   typedef typename Mesh::FaceIterator FaceIterator;
   typedef typename Mesh::CellIterator CellIterator;
+
+  typedef std::map<I, std::set<I> > PDS; // 并行网格数据结构
   typedef json MeshInfo;
+
 public:
-  ParallelMesh(int id, std::string & name, std::string & ptype="node")
+  ParallelMesh(int id, std::string & ptype="node")
   {
-    m_info["name"] = name;
-    m_info["ptype"] = ptype;
     m_info["id"] = id;
+    m_info["ptype"] = ptype;
+
+    m_info["cpid"] = std::vector<I>(); // 单元所在进程编号
+    m_info["npid"] = std::vector<I>(); // 节点所在进程编号
+
+    m_info["cgid"] = std::vector<I>(); // 单元的全局编号
+    m_info["ngid"] = std::vector<I>(); // 节点的全局编号
+
+    m_info["cg2l"] = std::map<I, I>(); // 单元全局到局部编号的映射
+    m_info["ng2l"] = std::map<I, I>(); // 节点全局到局部编号的映射
+
+    m_info["pds"]["node"] = PDS();
+    m_info["pds"]["cell"] = PDS();
+  }
+
+  void construct_parallel_data_structure()
+  {
+
+    auto & ptype = m_info["ptype"];
+    auto id = m_info["id"];
+    if(ptype == "node")
+    {
+      Toplogy node2node;
+      node_to_node(node2node);
+      auto & loc = node2node.locations();
+      auto & nei = node2node.neighbors();
+      auto & pds = parallel_data_structure();// 本网格需要发送信息的节点 
+      auto NN = number_of_nodes();
+      auto & npid = node_process_id();
+      auto & gid = node_global_id();
+      auto & ng2l = node_global_to_local_id();
+      for(I i=0; i < NN; i++)
+      {
+        ng2l[gid[i]] = i;
+        if(npid[i] != id) // i 是其他进程的点, 那么他相邻的点也是 nid[j] 网格的点
+        {
+          for(int k = loc[i]; k < loc[i+1]; k++)// 循环 i 相邻的点, 将本进程的点放入pds0[nid[j]] 中
+          {
+            if(npid[k] == id) // k 是本进程的点
+            {
+              pds[npid[i]].insert(k);
+            }
+          }
+        }
+      }
+    }
+    else if(ptype == "cell")
+    {
+    }
+  }
+
+  std::map<I, I> & cell_global_to_local_id()
+  {
+    return m_info["cg2l"];
+  }
+
+  std::map<I, I> & node_global_to_local_id()
+  {
+    return m_info["ng2l"];
   }
 
   std::vector<I> & cell_global_id()
   {
+    return m_info["cgid"]
   }
   
   std::vector<I> & node_global_id()
   {
-    return m_node_gid;
+    return m_info["ngid"];
   }
 
-  ParallelDataStructure & parallel_data_structure()
+  std::vector<I> & node_process_id()
   {
-    return m_pds;
+    return m_info["npid"];
+  }
+
+  std::vector<I> & cell_process_id()
+  {
+    return m_info["cpid"];
+  }
+
+  PDS & parallel_data_structure()
+  {
+    auto & ptype = m_info["ptype"];
+    return m_info["pds"][ptype];
   }
 private:
   MeshInfo m_info;
--- test/parallel_coloring.cpp
+++ test/parallel_coloring.cpp
@@ -8,15 +8,17 @@
 
 #include "geometry/Geometry_kernel.h"
 #include "mesh/TriangleMesh.h"
+#include "mesh/ParallelMesh.h"
 #include "mesh/VTKMeshReader.h"
 
 typedef WHYSC::Geometry_kernel<double, int> GK;
 typedef GK::Point_3 Node;
 typedef GK::Vector_3 Vector;
 typedef WHYSC::Mesh::TriangleMesh<GK, Node, Vector> TriMesh;
-typedef TriMesh::Cell Cell;
-typedef TriMesh::Toplogy Toplogy;
-typedef WHYSC::Mesh::VTKMeshReader<TriMesh> Reader;
+typedef WHYSC::Mesh::ParallelMesh<GK, TriMesh> PMesh;
+typedef PMesh::Cell Cell;
+typedef PMesh::Toplogy Toplogy;
+typedef WHYSC::Mesh::VTKMeshReader<PMesh> Reader;
 
 
 void mesh_coloring(TriMesh & mesh, std::vector<int> & color)
@@ -28,12 +30,14 @@ void mesh_coloring(TriMesh & mesh, std::vector<int> & color)
   auto NN = mesh.number_of_nodes();
   auto NE = mesh.number_of_edges();
 
+  color.resize(NN);
+
   std::vector<int> randVal(NN);
   std::vector<bool> isMin(NN, true);
 
   std::list<int> edges; //没有被删除的边
   std::list<int> nColored; //没有被染色的点
-  /*auto & pds = mesh.parallel_data_structure();
+  auto & pds = mesh.parallel_data_structure();
 
   for(auto idx:pds[rank])
     nColored.push_back(idx);
@@ -50,16 +54,19 @@ void mesh_coloring(TriMesh & mesh, std::vector<int> & color)
   {
     cmax++;
      // 只对没有染色的点进行循环
-    for(const int& index: nColored)
+    for(const int& idx: nColored)
     {
-      isMin[index] = true; //初始所有未染色点都是最小点
-      randVal[index] = rand();
+      isMin[idx] = true; //初始所有未染色点都是最小点
+      randVal[idx] = rand();
     }
 
     // 通信影像节点上的随机值
+    for(const auto & [key, value]:pds)
+    {
+    }
+    
 
   }
-  */
 
 }
 
@@ -76,54 +83,20 @@ int main(int argc, char * argv[])
 
   std::cout << "process " << rank << " of " << nprocs << " process." 
     << "The file name is " << fname <<  std::endl;
-  TriMesh mesh;
+
+  PMesh mesh(rank, "node");
   Reader reader(&mesh);
   reader.read(ss.str());
-/*
-  std::vector<int> nid;
-  reader.get_node_data("nid", nid);
+
+  auto & npid = mesh.node_process_id();
+  reader.get_node_data("nid", npid);
+
   auto & gid = mesh.node_global_id();
   reader.get_node_data("gid", gid);
 
-  // 网格拓扑信息
-  Toplogy node2node;
-  mesh.node_to_node(node2node);
-  auto & loc = node2node.locations();
-  auto & nei = node2node.neighbors();
-  auto & pds = mesh.parallel_data_structure();// 本网格中其它进程的点
-  std::map<int, std::set<int> > pds0; // 本进程节点在其它网格的点
-  std::map<int, int> gid2lid; // 全局编号到局部编号
-  for(int j = 0; j < nid.size(); j++)
-  {
-    pds[nid[j]].push_back(j);
-    if(nid[j] != rank) // j 是其他进程的点, 那么他相邻的点也是 nid[j] 网格的点
-    {
-      for(int k = loc[j]; k < loc[j+1]; k++)// 循环 j 相邻的点, 将本进程的点放入pds0[nid[j]] 中
-      {
-        if(nid[k] == rank) // k 是本进程的点
-        {
-          pds0[nid[j]].insert(k);
-        }
-      }
-    }
-    gid2lid[gid[j]] = j;
-  }
-
-  for(int j = 0; j < nprocs; j++)
-  {
-    if(j != rank)
-    {
-      for(auto k : pds[j])
-      {
-
-      }
-    }
-  }
-
-  auto NN = mesh.number_of_nodes();
-  std::vector<int> color(NN, 0); 
-  //mesh_coloring(mesh, color);
- */
+  mesh.construct_parallel_data_structure();
+  std::vector<int> color(); 
+  mesh_coloring(mesh, color);
 
   MPI_Finalize();
   return 0;
